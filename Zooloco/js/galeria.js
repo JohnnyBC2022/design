66a0d9ade289f7efb67009129df403d6476d1148
JohnnyBC2022 <jonathanbc2012@gmail.com>
2022-03-24 19:11:40 +0100

    12

---
+++ Zooloco/js/galeria.js
@@ -0,0 +1,65 @@
+var cabezas = [
+    'Elefante-cabeza.png',
+    'Oso-cabeza.png',
+    'Perro-cabeza.png'
+];
+
+var cuerpos = [
+    'Elefante-cuerpo.png',
+    'Oso-cuerpo.png',
+    'Perro-cuerpo.png',
+];
+
+var patas = [
+    'Elefante-patas.png',
+    'Oso-patas.png',
+    'Perro-patas.png'
+];
+
+var n_cabeza = 0;
+var n_cuerpo = 0;
+var n_pata = 0;
+
+
+function cabeza() {
+    if (n_cabeza < cabezas.length - 1) {
+        n_cabeza++;
+    } else {
+        n_cabeza = 0;
+    }
+    document.getElementById('cabezas').style.backgroundImage = "url('img/" + cabezas[n_cabeza] + "')";
+}
+
+function cuerpo() {
+    if (n_cuerpo < cuerpos.length - 1) {
+        n_cuerpo++;
+    } else {
+        n_cuerpo = 0;
+    }
+    document.getElementById('cuerpos').style.backgroundImage = "url('img/" + cuerpos[n_cuerpo] + "')";
+}
+
+function pata() {
+    if (n_pata < patas.length - 1) {
+        n_pata++;
+    } else {
+        n_pata = 0;
+    }
+    document.getElementById('patas').style.backgroundImage = "url('img/" + patas[n_pata] + "')";
+}
+
+function aleatorio() {
+    document.getElementById('cabezas').style.backgroundImage = "url('img/" + cabezas[random(0, cabezas.length - 1)] + "')";
+    document.getElementById('cuerpos').style.backgroundImage = "url('img/" + cuerpos[random(0, cuerpos.length - 1)] + "')";
+    document.getElementById('patas').style.backgroundImage = "url('img/" + patas[random(0, patas.length - 1)] + "')";
+}
+
+
+
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+aleatorio();
